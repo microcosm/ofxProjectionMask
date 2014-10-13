adf10416b86ff77ea9a8184992e8f11752914638
Andy McWilliams <andy@jahya.net>
2014-10-13 01:09:37 -0400

    Adds white pattern

--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -3,6 +3,7 @@
 #include "ofMain.h"
 #include "ofxProjectionMapper.h"
 #include "HueCyclePattern.h"
+#include "SetupPattern.h"
 
 class ofApp : public ofBaseApp{
 
@@ -22,5 +23,15 @@ public:
     void gotMessage(ofMessage msg);
 		
     ofxProjectionMapper mapper;
+    
+    /* You can only have one 'pattern' at a time, but the pattern
+       can have multiple FBOs. Uncomment these one at a time to
+       get an example, and use these classes as templates for your
+       own patterns: */
+    
+    /* Cycle through colors, good test pattern */
     HueCyclePattern pattern;
+    
+    /* Just plain white, good for checking the edges of your shapes */
+    //SetupPattern pattern;
 };
