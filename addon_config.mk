686e44cb813199861ab2af2b077008ab75ebc4f0
Andy McWilliams <andy@jahya.net>
2014-10-19 00:46:00 -0400

    Adds config.mk

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxProjectionMask
+	ADDON_DESCRIPTION = Projection mapping without the mapping
+	ADDON_AUTHOR = Andrew McWilliams
+	ADDON_TAGS = mask projection-mask projection-map projection-mapping
+	ADDON_URL = http://github.com/microcosm/ofxProjectionMask
+
+common:
+	ADDON_DEPENDENCIES = ofxXmlSettings ofxTriangle
